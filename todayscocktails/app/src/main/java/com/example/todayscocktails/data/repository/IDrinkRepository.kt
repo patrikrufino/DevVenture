e1a205e4f9a19bdef3aef6bd90f31945ca7a9c4a
Lucas Sampaio <lukaz.sampaio@gmail.com>
2021-06-27 13:30:11 -0300

    Consultar item pelo nome
    O código está quebrado. Não fazer merge.

--- todayscocktails/app/src/main/java/com/example/todayscocktails/data/repository/IDrinkRepository.kt
+++ todayscocktails/app/src/main/java/com/example/todayscocktails/data/repository/IDrinkRepository.kt
@@ -1,7 +1,9 @@
 package com.example.todayscocktails.data.repository
 
+import com.example.todayscocktails.DrinkDetails
 import com.example.todayscocktails.data.model.DrinkRemoteEntity
 
 interface IDrinkRepository {
     suspend fun getAllDrinks(): List<DrinkRemoteEntity>?
+    suspend fun getDrinkByName(name: String): DrinkDetails?
 }
--- todayscocktails/app/src/main/java/com/example/todayscocktails/viewmodel/DrinkViewModel.kt
+++ todayscocktails/app/src/main/java/com/example/todayscocktails/viewmodel/DrinkViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todayscocktails.DrinkDetails
 import com.example.todayscocktails.data.model.DrinkRemoteEntity
 import com.example.todayscocktails.data.repository.DrinkRepository
 import kotlinx.coroutines.launch
@@ -11,6 +12,7 @@ import kotlinx.coroutines.launch
 class DrinkViewModel(private val repository: DrinkRepository) : ViewModel() {
 
     private var drinks: MutableLiveData<List<DrinkRemoteEntity>> = MutableLiveData()
+    private var drinkDetails: DrinkDetails? = null
 
     fun getDrinks(): LiveData<List<DrinkRemoteEntity>> {
         viewModelScope.launch {
@@ -18,5 +20,12 @@ class DrinkViewModel(private val repository: DrinkRepository) : ViewModel() {
             drinks.postValue(result)
         }
         return drinks
+    }
+
+    fun getDrinkByName(name: String): DrinkDetails? {
+        viewModelScope.launch {
+            drinkDetails = repository.getDrinkByName(name)
+        }
+        return drinkDetails
     }
 }
